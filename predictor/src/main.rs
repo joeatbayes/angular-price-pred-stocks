9aa53a7e66a1388925c84fa87f5f975609e0a9da
joeatbayes <joexdobs@gmail.com>
2022-08-29 13:40:12 -0500

    working on testing basic idea

--- predictor/src/main.rs
+++ predictor/src/main.rs
@@ -10,49 +10,74 @@ use linreg::{linear_regression};
 // time frames.  It is also modified to penalize
 // large differences from the line even if there 
 // is a matching set on the other side of the line.
-fn reg_line_fit_err(darr : &[f32], offset : usize, slope_rat :f32) -> f32 {
+fn reg_line_fit_err(darr : &[f32], offset : f32, slope_rat :f32) -> f32 {
     let nele = darr.len();
     if nele < 2 {
         return -1.0;
     }
     let slope_unit = (offset as f32) * slope_rat;
-    let mut cmp_val = darr[offset];
+    let mut cmp_val = offset as f32;
     let mut err_sum = 0.0;
     for bndx in 0 .. nele {
         let pval = darr[bndx];
         let pdif = pval - cmp_val;
         let dif_rat = pdif / cmp_val;
         let abs_dif_rat = dif_rat.abs();
-        let psqr = abs_dif_rat * abs_dif_rat;
-        err_sum += psqr;
+        err_sum += abs_dif_rat;
+        //println!("ndx={0:#?} pval={1:#?} cmp_val={2:#?} dif={3:#?} dif_rat={4:#?}", bndx,pval,cmp_val,pdif, dif_rat);
         cmp_val = cmp_val + slope_unit;
     }
-    return err_sum / (nele as f32);
+    return err_sum / (nele as f32); 
+}
+
+#[derive(Debug)]
+pub struct BestNumDayFit {
+    pub sloper : f32,
+    pub num_ele: i32,
+    pub err : f32,
+    pub offset : f32
 }
 
 //TODO:  Use enum parameter to select the data open,low,high,low,close
 //TODO:  use a getSlice which implements the enum logic so we don't spread it around.
 //
-fn find_best_fit_in_range(pbars : bars::Bars, offset : usize, min_len : i32, max_len : i32) -> i32 {
+fn find_best_fit_in_range(pbars : bars::Bars, end_ndx : usize, min_len : i32, max_len : i32) -> BestNumDayFit {
     // Start at a current day then work backwards to find the trend length
     // between min,max days that yields the lowest error.
+    let mut best : BestNumDayFit = BestNumDayFit {sloper: 0.0, 
+           num_ele : min_len, 
+           err : 99999999.99, 
+           offset : 0.0 };
+   
     for num_day in min_len .. max_len {
-        let beg_day = (num_day as usize) - offset;
-        let dayns = pbars.slice_dayn(beg_day, offset);
-        let closes= pbars.slice_close(beg_day,offset);
-        let val_at_offset = pbars.close[offset];
-        println!("dayns={0:#?} closes={1:#?} ", dayns, closes); 
+        println!("numDay={0:#?} min_len={1:#?} max_len={2:#?}", num_day, min_len, max_len);
+        let beg_ndx = end_ndx - (num_day as usize);
+        let dayns = pbars.slice_dayn(0, num_day as usize);
+        let closes= pbars.slice_close(beg_ndx, end_ndx);
+        let beg_ndx_val = pbars.close[beg_ndx];
+        best.num_ele = num_day;
+        //println!("dayns={0:#?} closes={1:#?} beg_ndx_val={2:#?} ", dayns, closes, beg_ndx_val); 
         let tpl1 : (f32, f32) = linear_regression(dayns, closes).unwrap_or((0.0,-0.001));
-        println!("tpl1={0:#?}", tpl1);
+        //println!("numDay={0:#?},tpl1={01:#?}", num_day, tpl1);
   
         let (slope, offset) = tpl1;
-        let slope_rat = slope / val_at_offset;
+        let slope_rat = slope / beg_ndx_val;
         println!("slope={0:#?}, offset={1:#?}, slope_ratio={2:#?}", slope, offset, slope_rat);
         let days_offset = 350;
-        let fit_err = reg_line_fit_err(dayns, days_offset, slope_rat);
-        println!("fit_err={0:#?}", fit_err);
+        let fit_err = reg_line_fit_err(closes, beg_ndx_val, slope_rat);
+        println!("fit_err={0:#?} best.err={1:#?} nday={2:#?}", fit_err, best.err, num_day);
+        if fit_err < best.err {
+            best.err = fit_err;
+            best.num_ele = num_day;
+            best.sloper = slope_rat;
+            best.offset = offset;
+            println!("best_fit{0:#?}", best)
+            //println!("new best num_day={0:#?} err={1:#?} slope={2:#?} offset={3:#?}",
+            //    num_day, best.err, best.sloper, best.offset)
+        }
+        println!("best_fit_num_day={0:#?}", best.num_ele)
     }
-    return 1;
+    return best;
 }
 
   
@@ -77,7 +102,7 @@ fn main() {
     let slope_rat = slope / offset;
     println!("slope={0:#?}, offset={1:#?}, slope_ratio={2:#?}", slope, offset, slope_rat);
 
-    let bfl = find_best_fit_in_range(pbars, 500, 10,  30); 
+    let bfl = find_best_fit_in_range(pbars, 500, 7,  180); 
     println!("bfl={0:#?}", bfl);
     
     
