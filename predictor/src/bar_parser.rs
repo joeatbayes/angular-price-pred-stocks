f7bc1299cc0fb53cf1555237e0b934f1daa0ef88
joeatbayes <joexdobs@gmail.com>
2022-08-28 22:59:45 -0500

    basically working with linear regression and calc_err working

--- predictor/src/bar_parser.rs
+++ predictor/src/bar_parser.rs
@@ -5,6 +5,7 @@ pub mod bar_parser {
     use std::fs::File;
     use std::io::{self, BufRead};
     use std::path::Path;
+    //use num_traits::cast::ToPrimitive;
     //use std::collections::HashMap;
 
     // The output is wrapped in a Result to allow matching on errors
@@ -15,8 +16,10 @@ pub mod bar_parser {
         Ok(io::BufReader::new(file).lines())
     }
 
+  
     #[derive(Debug)]
-    pub struct Bars {
+    pub struct Bars { 
+        dayn : Vec<f32>,
         date : Vec<String>,
         open : Vec<f32>,
         high : Vec<f32>,
@@ -29,6 +32,7 @@ pub mod bar_parser {
     impl Bars {
         pub fn new() -> Bars {
             Bars {
+               dayn: Vec::new(),
                date: Vec::new(),
                open: Vec::new(),
                high: Vec::new(),
@@ -42,8 +46,11 @@ pub mod bar_parser {
         pub fn len(&self) -> usize {
             self.date.len()
         } 
-        
-        pub fn slice(&self, begn: usize, endn :usize) -> (&[String], &[f32]) {
+ 
+        // ensure start and end indexes for a slice will not exceed
+        // the slide or do silly things like ask for a end after the 
+        // start.
+        pub fn fix_slice_parm(&self, begn : usize, endn: usize) -> (usize, usize) {
             let slen = self.len();
 
             let endn =  if  endn >= slen { slen -1}
@@ -52,11 +59,26 @@ pub mod bar_parser {
             let startn = if begn <= 0 {0}
               else if begn > endn {endn}
               else {begn};
-            
-            
-            //if endn 
-            return (&self.date[startn..endn], &self.open[startn..endn]);
+
+              return (startn, endn)
         }
+
+        pub fn slice_dayn(&self, begn: usize, endn :usize) -> &[f32] {
+            let (startn, endn) = self.fix_slice_parm(begn, endn);            
+            return &self.dayn[startn..endn];
+        }
+
+        pub fn slice_date(&self, begn: usize, endn :usize) -> &[String] {
+            let (startn, endn) = self.fix_slice_parm(begn, endn);            
+            return &self.date[startn..endn];
+        }
+
+        pub fn slice_close(&self, begn: usize, endn :usize) ->  &[f32] {
+            let (startn, endn) = self.fix_slice_parm(begn, endn);            
+            return &self.close[startn..endn];
+        }
+
+
     }
 
 
@@ -67,7 +89,7 @@ pub mod bar_parser {
         println!("fiName={0:#?}",fname);
         if let Ok(lines) = read_lines(&fname) {
             // Consumes the iterator, returns an (Optional) String   
-            let mut lc = 0;
+            let mut lc : i32 = 0;
             for line in lines {
                 lc = lc + 1;
                 if lc <= 1 {
@@ -80,6 +102,7 @@ pub mod bar_parser {
                     let sp: Vec<&str> = uwline.split(",").collect();
                     if sp.len() >= 7 {
                         //println!("date={0:#?} open={1:#?} ", &sp[0], &sp[1]); 
+                        dta.dayn.push(lc as f32);
                         dta.date.push(sp[0].to_string());
                         dta.open.push(sp[1].parse().unwrap_or(-1.0));
                         dta.high.push(sp[2].parse().unwrap_or(-1.0)); 
--- predictor/src/main.rs
+++ predictor/src/main.rs
@@ -1,6 +1,6 @@
 pub mod bar_parser;
-//pub use crate::bar_parser as parser;
 use crate::bar_parser::bar_parser as parser;
+use linreg::{linear_regression};
 
 fn main() {
     println!("Hello, world!");
@@ -8,6 +8,87 @@ fn main() {
     let hm = parser::read_file(&"../data/bars/SPY.csv");
     println!("hm={0:#?}", hm);
 
-    let (dts, opens) = hm.slice(1,15);
-    println!("dts={0:#?} opens={1:#?} ", dts, opens);
+    // TODO: change this so slice asks for columns by enum
+    //  which can return vec of int, float, string depending
+    //  on what enum is asked for. // https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
+    let dayns = hm.slice_dayn(1,350);
+    let closes= hm.slice_close(1,350);
+    println!("dayns={0:#?} closes={1:#?} ", dayns, closes);
+     
+    //let xs: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    //let ys: Vec<f64> = vec![2.0, 4.0, 5.0, 4.0, 5.0];
+    //assert_eq!(Ok((0.6, 2.2)), linear_regression(&xs, &ys));
+   
+    let x: (f32, f32) = (12.0,18.0);
+    //let x: (f32, f32)  = linear_regression(dayns, closes).unwrap(); 
+    //let  y: Result((f32, f32)) = linear_regression(dayns, closes);  
+    
+    let tpl1 : (f32, f32) = linear_regression(dayns, closes).unwrap_or((0.0,-0.001));
+    println!("tpl1={0:#?}", tpl1);
+
+    let (slope, offset) = tpl1;
+    let slope_rat = slope / offset;
+    println!("slope={0:#?}, offset={1:#?}, slope_ratio={2:#?}", slope, offset, slope_rat);
+
+        let tpl : (f32, f32) = match linear_regression(dayns, closes) {    
+        Ok(tpl) => { println!("val={0:#?}", tpl);
+                      tpl 
+                   },
+        _ => { println!("not ok"); 
+             (0.0,-0.001)
+            }
+    };
+
+    // Compute a fit fitness value that can be used
+    // to compute how well the data points from a 
+    // linear regression cluster around the line. It 
+    // is specifically modified to work with ratio of
+    // difference to allow comparison across different 
+    // time frames.  It is also modified to penalize
+    // large differences from the line even if there 
+    // is a matching set on the other side of the line.
+     
+    fn calc_err(darr : &[f32], offset : f32, slope_rat :f32) -> f32 {
+       let nele = darr.len();
+       let slope = offset * slope_rat;
+       let cmp_val = offset;
+       let mut err_sum = 0.0;
+       for bndx in 0 .. nele {
+           let pval = darr[bndx];
+           let pdif = pval - cmp_val;
+           let dif_rat = pdif / cmp_val;
+           let abs_dif_rat = dif_rat.abs();
+           let psqr = abs_dif_rat * abs_dif_rat;
+           err_sum += psqr;
+           let cmp_val = cmp_val + slope;
+       }
+       return err_sum / (nele as f32);
+    }
+
+    /*
+    // Start at a current day then work backwards to find the trend length
+    // between min,max days that yields the lowest error.
+    let short_min_days = 10
+    let short_max_days = 30
+    let day_ndx = 350 
+    for num_day in short_min_days .. short_max_days {
+        let beg_day = day_ndx - num_day
+        let dayns = hm.slice_dayn(beg_day, day_ndx);
+        let closes= hm.slice_close(beg_day,day_ndx);
+        println!("dayns={0:#?} closes={1:#?} ", dayns, closes); 
+        let tpl1 : (f32, f32) = linear_regression(dayns, closes).unwrap_or((0.0,-0.001));
+        println!("tpl1={0:#?}", tpl1);
+    
+        let (slope, offset) = tpl1;
+        let slope_rat = slope / offset;
+        println!("slope={0:#?}, offset={1:#?}, slope_ratio={2:#?}", slope, offset, slope_rat);
+        
+
+
+        
+    } 
+    */
+    
+   
+
 }
